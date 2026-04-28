693a209d68a39dd4bfd41933b8aafbb145084928
passplease <1175851421@qq.com>
2026-04-28 09:45:28 +0800

    Support Create 6.0.10

--- src/main/java/net/smartercontraptionstorage/Mixin/Contraption/MountedStorageManagerMixin.java
+++ src/main/java/net/smartercontraptionstorage/Mixin/Contraption/MountedStorageManagerMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.resources.RegistryOps;
 import net.smartercontraptionstorage.AddStorage.FluidHander.DumpHandler;
 import net.smartercontraptionstorage.AddStorage.FluidHander.FluidHandlerHelper;
 import net.smartercontraptionstorage.AddStorage.FluidHander.MovingFluidStorage;
@@ -71,13 +72,13 @@ public interface MountedStorageManagerMixin {
         }
 
         @Inject(method = "lambda$read$7",at = @At("HEAD"),remap = false)
-        public void writePos1(CompoundTag tag, CallbackInfo ci){
+        public void writePos1(RegistryOps registryOps, CompoundTag tag, CallbackInfo ci){
             Optional<BlockPos> pos = NbtUtils.readBlockPos(tag, "pos");
             pos.ifPresent(blockPos -> tag.getCompound("storage").getCompound(MovingItemStorageType.TYPE).put(MovingItemStorageType.TAG, NbtUtils.writeBlockPos(blockPos)));
         }
 
         @Inject(method = "lambda$read$10",at = @At("HEAD"),remap = false)
-        public void writePos2(CompoundTag tag, CallbackInfo ci){
+        public void writePos2(RegistryOps registryOps, CompoundTag tag, CallbackInfo ci){
             Optional<BlockPos> pos = NbtUtils.readBlockPos(tag, "pos");
             pos.ifPresent(blockPos -> tag.getCompound("storage").getCompound(MovingFluidStorageType.TYPE).put(MovingItemStorageType.TAG, NbtUtils.writeBlockPos(blockPos)));
         }
